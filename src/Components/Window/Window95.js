a43e16154d287f966c9c59b5eec51a702676b5ad
Federico <fedeacos045@gmail.com>
2023-06-25 15:40:27 -0300

    first commit

---
+++ src/Components/Window/Window95.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Windows95.css";
+
+const Window95 = () => {
+  return (
+    <div>
+      <div class="container">
+        <img src="left-hand.png" alt="left-hand" class="left-hand" />
+        <img src="right-hand.png" alt="right-hand" class="right-hand" />
+      </div>
+      <img src="band.png" alt="band" class="band" />
+      <div class="wrapper">
+        <div>
+          <div class="header">Move bitch</div>
+          <ul>
+            <li>
+              <h1>Fechas</h1>
+            </li>
+            <li>
+              <h3>Fecha 1</h3>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Window95;
