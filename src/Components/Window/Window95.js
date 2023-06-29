68279a9c08f5f57b337b9018a4b70cbd29014ea4
Federico <fedeacos045@gmail.com>
2023-06-28 22:17:40 -0300

    fixed images

--- src/Components/Window/Window95.js
+++ src/Components/Window/Window95.js
@@ -3,25 +3,12 @@ import "./Windows95.css";
 
 const Window95 = () => {
   return (
-    <div>
-      <div class="container">
+    <div class="screen-container">
+      <div class="hands-container">
         <img src="left-hand.png" alt="left-hand" class="left-hand" />
         <img src="right-hand.png" alt="right-hand" class="right-hand" />
       </div>
       <img src="band.png" alt="band" class="band" />
-      <div class="wrapper">
-        <div>
-          <div class="header">Move bitch</div>
-          <ul>
-            <li>
-              <h1>Fechas</h1>
-            </li>
-            <li>
-              <h3>Fecha 1</h3>
-            </li>
-          </ul>
-        </div>
-      </div>
     </div>
   );
 };
