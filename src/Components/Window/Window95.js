0be680dfd2fd0c9db28d67dee827010de275fe53
Pablo Arnaez <pabloarnaez87@gmail.com>
2023-07-01 00:44:37 -0300

    modificacion titulo

--- src/Components/Window/Window95.js
+++ src/Components/Window/Window95.js
@@ -4,6 +4,7 @@ import "./Windows95.css";
 const Window95 = () => {
   return (
     <div class="screen-container">
+       <h1>COSA NOSTRA</h1>
       <div class="hands-container">
         <img src="left-hand.png" alt="left-hand" class="left-hand" />
         <img src="right-hand.png" alt="right-hand" class="right-hand" />
