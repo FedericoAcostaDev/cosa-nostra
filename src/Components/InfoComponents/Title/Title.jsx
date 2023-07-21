c881c8793e64a534a4b419115e801e68d5f4b9f3
Federico <fedeacos045@gmail.com>
2023-07-21 04:51:15 -0300

    Improved architect project architect (structure for components) and changed title to a img

---
+++ src/Components/InfoComponents/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Title.css";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <img src="./title1.png" alt="band title" />
+    </div>
+  );
+};
+
+export default Title;
